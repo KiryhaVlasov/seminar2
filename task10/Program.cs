e76df7a87535aa933c83f16b399ef822d4a01b7c
KiryhaVlasov <m45362388@gmail.com>
2022-05-19 13:56:45 +0300

    task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10: Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+// Примеры:
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine($"Введите трехзначное число");
+string num = Convert.ToString(Console.ReadLine());
+Console.Write(num.ToString()[1]);
